d077fc28c021dfd40cd29dda3fd71e4c35044628
fauzprojecto <fauzavia103@gmail.com>
2025-12-22 11:20:02 +0700

    Update scrol.js

--- scrol.js
+++ scrol.js
@@ -23,7 +23,7 @@ gsap.to(heroTitle, {
   scrollTrigger: {
     trigger: ".spacer",        
     start: "top 60%",          
-    end: "top 10%",
+    end: "top 20%",
     scrub: true                
   }
 });
@@ -37,7 +37,7 @@ gsap.utils.toArray(".spaceb").forEach(box => {
   ScrollTrigger.create({
     trigger: box,
     start: "top 75%",
-    end: "bottom 25%",
+    end: "bottom 45%",
 
     onEnter: () => {
       gsap.to(box, {
@@ -51,10 +51,10 @@ gsap.utils.toArray(".spaceb").forEach(box => {
       gsap.to(chars, {
         opacity: 1,
         y: 0,
-        duration: 0.35,
+        duration: 0.25,
         ease: "power2.out",
         stagger: 0.015,
-        delay: 0.2
+        delay: 0.1
       });
     },
 
@@ -62,11 +62,11 @@ gsap.utils.toArray(".spaceb").forEach(box => {
       
       gsap.to(chars, {
         opacity: 0,
-        y: 20,
+        y: 5,
         duration: 0.2,
         ease: "power1.in",
         stagger: {
-          each: 0.01,
+          each: 0.001,
           from: "end"
         }
       });
@@ -84,3 +84,74 @@ gsap.utils.toArray(".spaceb").forEach(box => {
 });
 
 window.addEventListener("load", () => ScrollTrigger.refresh());
+const contents = gsap.utils.toArray(".spacer2 .spacew2"); 
+
+gsap.to(contents, {
+  xPercent: -100 * (contents.length - 1), 
+  ease: "none",
+  scrollTrigger: {
+  trigger: ".spacer2", 
+  pin: true, 
+  scrub: 1, 
+
+  }
+});
+
+gsap.utils.toArray(".spacew2").forEach(section => {
+  const lets = section.querySelectorAll(".lest span");
+  const git = section.querySelector(".git");
+
+  if (!lets.length || !git) return;
+
+  gsap.set(lets, { opacity: 0, y: 20 });
+  gsap.set(git, { opacity: 0, y: 20 });
+
+  ScrollTrigger.create({
+    trigger: section,
+    start: "top 50%",
+    end: "bottom 30%",
+
+    onEnter: () => {
+      gsap.to(lets, {
+        opacity: 1,
+        y: 0,
+        duration: 0.6,
+        ease: "power2.out",
+        stagger: {
+          each: 0.03,
+          from: "start"
+        }
+      });
+
+      gsap.to(git, {
+        opacity: 1,
+        y: 0,
+        duration: 0.8,
+        ease: "power2.out",
+        delay: 0.1
+      });
+    },
+
+    onLeaveBack: () => {
+      gsap.to(git, {
+        opacity: 0,
+        y: 20,
+        duration: 0.3,
+        ease: "power2.in"
+      });
+
+      gsap.to(lets, {
+        opacity: 0,
+        y: 20,
+        duration: 0.2,
+        ease: "power1.in",
+        stagger: {
+          each: 0.01,
+          from: "end"
+        }
+      });
+    }
+  });
+});
+
+
